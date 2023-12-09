b2a67606895568d3e0c76eaa2178749ea96b7fb8
ScrimaliAnthony <anthony.scrimali@gmail.com>
2023-12-09 10:59:10 +0100

    Types - any

---
+++ Types/src/any.ts
@@ -0,0 +1,9 @@
+// Le type any supprime l'utilité de typescript à typer les variables
+let test: any = "coucou";
+console.log(test);
+test = 5;
+console.log(test);
+
+// Ici, il est possible de prendre une variable et de lui assigner un string puis un number
+
+// Il est possible d'avoir un type any de manière implicite si le type n'a pas été écrit sauf si le mode strict est activé. Il est recommandé de ne pas utiliser le type any mais plutôt le type unknown
